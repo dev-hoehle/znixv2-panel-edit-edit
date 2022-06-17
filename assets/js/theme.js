89cc7190363127bfd2b2a9a7d012529d98456cb8
anditv21 <75949923+anditv21@users.noreply.github.com>
2022-06-17 22:51:03 +0200

    Revert "✨ code formatting"
    This reverts commit d98f82389f3e08487e21a97053577da26d39d258.

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,61 +1,53 @@
-(function () {
+(function() {
   "use strict"; // Start of use strict
 
-  var sidebar = document.querySelector(".sidebar");
+  var sidebar = document.querySelector('.sidebar');
 
-  var sidebarToggles = document.querySelectorAll(
-    "#sidebarToggle, #sidebarToggleTop"
-  );
-
+  var sidebarToggles = document.querySelectorAll('#sidebarToggle, #sidebarToggleTop');
+  
   if (sidebar) {
-    var collapseEl = sidebar.querySelector(".collapse");
-    var collapseElementList = [].slice.call(
-      document.querySelectorAll(".sidebar .collapse")
-    );
+    
+    var collapseEl = sidebar.querySelector('.collapse');
+    var collapseElementList = [].slice.call(document.querySelectorAll('.sidebar .collapse'))
     var sidebarCollapseList = collapseElementList.map(function (collapseEl) {
       return new bootstrap.Collapse(collapseEl, { toggle: false });
     });
 
     for (var toggle of sidebarToggles) {
+
       // Toggle the side navigation
-      toggle.addEventListener("click", function (e) {
-        document.body.classList.toggle("sidebar-toggled");
+      toggle.addEventListener('click', function(e) {
+        document.body.classList.toggle('sidebar-toggled');
 
-        sidebar.classList.toggle("toggled");
+          sidebar.classList.toggle('toggled');
 
-        if (sidebar.classList.contains("toggled")) {
+        if (sidebar.classList.contains('toggled')) {
           for (var bsCollapse of sidebarCollapseList) {
             bsCollapse.hide();
           }
-        }
+        };
       });
     }
 
     // Close any open menu accordions when window is resized below 768px
-    window.addEventListener("resize", function () {
-      var vw = Math.max(
-        document.documentElement.clientWidth || 0,
-        window.innerWidth || 0
-      );
+    window.addEventListener('resize', function() {
+      var vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
 
       if (vw < 768) {
         for (var bsCollapse of sidebarCollapseList) {
           bsCollapse.hide();
         }
-      }
+      };
     });
   }
 
   // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
-
-  var fixedNaigation = document.querySelector("body.fixed-nav .sidebar");
-
+  
+  var fixedNaigation = document.querySelector('body.fixed-nav .sidebar');
+  
   if (fixedNaigation) {
-    fixedNaigation.on("mousewheel DOMMouseScroll wheel", function (e) {
-      var vw = Math.max(
-        document.documentElement.clientWidth || 0,
-        window.innerWidth || 0
-      );
+    fixedNaigation.on('mousewheel DOMMouseScroll wheel', function(e) {
+      var vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
 
       if (vw > 768) {
         var e0 = e.originalEvent,
@@ -66,19 +58,21 @@
     });
   }
 
-  var scrollToTop = document.querySelector(".scroll-to-top");
-
+  var scrollToTop = document.querySelector('.scroll-to-top');
+  
   if (scrollToTop) {
+    
     // Scroll to top button appear
-    window.addEventListener("scroll", function () {
+    window.addEventListener('scroll', function() {
       var scrollDistance = window.pageYOffset;
 
       //check if user is scrolling up
       if (scrollDistance > 100) {
-        scrollToTop.style.display = "block";
+        scrollToTop.style.display = 'block';
       } else {
-        scrollToTop.style.display = "none";
+        scrollToTop.style.display = 'none';
       }
     });
   }
+
 })(); // End of use strict
--- index.php
+++ index.php
@@ -1,52 +1,22 @@
 <?php
-require_once '../app/require.php';
-require_once '../app/controllers/CheatController.php';
-require_once '../app/controllers/AdminController.php';
-
-
-
+require_once 'app/require.php';
+require_once 'app/controllers/CheatController.php';
 
 $user = new UserController;
 $cheat = new CheatController;
-$admin = new AdminController;
 
 Session::init();
 
-Util::adminCheck();
+if (!Session::isLogged()) {
+    Util::redirect('./auth/login.php');
+}
 
 $username = Session::get("username");
 $uid = Session::get("uid");
-
 $sub = $user->getSubStatus();
 
 Util::banCheck();
-Util::head('Admin Panel');
-
-// if post request 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-
-    if (isset($_POST["cheatStatus"])) {
-        $admin->setCheatStatus();
-    }
-
-    if (isset($_POST["cheatMaint"])) {
-        $admin->setCheatMaint();
-    }
-
-    if (isset($_POST["cheatVersion"])) {
-        $ver = floatval($_POST['version']);
-        $admin->setCheatVersion($ver);
-    }
-
-    if (isset($_POST["sendmsg"])) {
-        $news = $_POST['msg'];
-        $admin->setnews($news);
-    }
-
-
-    header("location: index.php");
-}
+Util::head($username);
 
 
 ?>
@@ -57,20 +27,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Admin - Brand</title>
-    <link rel="icon" type="image/png" href="../favicon.png">
-    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+    <title>Dashboard - Brand</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="icon" type="image/png" href="favicon.png">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
-    <link rel="stylesheet" href="../assets/fonts/fontawesome-all.min.css">
-    <link rel="stylesheet" href="../assets/fonts/font-awesome.min.css">
-    <link rel="stylesheet" href="../assets/fonts/fontawesome5-overrides.min.css">
+    <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/fontawesome5-overrides.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
-    <link rel="stylesheet" href="../assets/css/untitled.css">
+    <link rel="stylesheet" href="assets/css/untitled.css">
 </head>
 
 <body id="page-top">
     <div id="wrapper">
-        <?php Util::adminNavbar(); ?>
+        <?php Util::navbar(); ?>
         <div class="d-flex flex-column" id="content-wrapper">
             <div id="content" style="background: #121421;">
                 <nav class="navbar navbar-light navbar-expand bg-white shadow mb-4 topbar static-top">
@@ -80,7 +50,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                                 <div class="shadow dropdown-list dropdown-menu dropdown-menu-end" aria-labelledby="alertsDropdown"></div>
                             </li>
                             <li class="nav-item dropdown no-arrow">
-                                <div class="nav-item dropdown no-arrow"><a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#"><span class="d-none d-lg-inline me-2 text-gray-600 small" style="color: #ffffff !important;"><?php Util::display(Session::get("username")); ?></span><img class="border rounded-circle img-profile" src="../assets/img/avatars/Portrait_Placeholder.png" style="border-color: rgb(255,255,255)!important;"></a>
+                                <div class="nav-item dropdown no-arrow"><a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#"><span class="d-none d-lg-inline me-2 text-gray-600 small" style="color: #ffffff !important;"><?php Util::display(Session::get("username")); ?></span><img class="border rounded-circle img-profile" src="assets/img/avatars/Portrait_Placeholder.png" style="border-color: rgb(255,255,255)!important;"></a>
                                     <div class="dropdown-menu shadow dropdown-menu-end animated--grow-in" style="background: #252935;border-style: none;margin-top: 11px;box-shadow: 0px 0px 3px 2px rgba(0,0,0,0.16)!important;"><a class="dropdown-item" href="profile.php" style="color: rgb(255,255,255);"><i class="fas fa-user fa-sm fa-fw me-2 text-gray-400" style="color: rgb(255,255,255)!important;"></i>&nbsp;Profile</a><a class="dropdown-item" id="logout" href="/auth/logout.php" style="color: rgb(255,255,255);"><i class="fas fa-sign-out-alt fa-sm fa-fw me-2 text-gray-400" style="color: rgb(255,255,255)!important;"></i>&nbsp;Logout</a></div>
                                 </div>
                             </li>
@@ -88,10 +58,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     </div>
                 </nav>
                 <div class="container-fluid">
-                    <div class="d-sm-flex justify-content-between align-items-center mb-4" data-aos="fade-down" data-aos-duration="1000">
+                    <div class="d-sm-flex justify-content-between align-items-center mb-4" data-aos="fade-down" data-aos-duration="800">
                         <h3 class="text-dark mb-0">Dashboard</h3>
                     </div>
-                    <div class="row" data-aos="fade-down" data-aos-duration="800">
+                    <div class="row" data-aos="fade-down" data-aos-duration="600">
                         <div class="col-md-6 col-xl-3 mb-4">
                             <div class="card shadow border-start-primary py-2" style="background: rgb(37,41,53);border-style: none;">
                                 <div class="card-body">
@@ -122,7 +92,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-6 col-xl-3 mb-4">
+                        <div class="col-md-6 col-xl-3 mb-5">
                             <div class="card shadow border-start-primary py-2" style="background: rgb(37,41,53);border-style: none;">
                                 <div class="card-body">
                                     <div class="row align-items-center no-gutters">
@@ -139,111 +109,79 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="row" data-aos="fade-down" data-aos-duration="600">
-                        <div class="col-md-6 col-xl-3 mb-4">
+
+                        <div class="col-md-6 col-xl-3 mb-4" id="SUBCOL">
                             <div class="card shadow border-start-primary py-2" style="background: rgb(37,41,53);border-style: none;">
                                 <div class="card-body">
                                     <div class="row align-items-center no-gutters">
                                         <div class="col me-2">
-                                            <div class="text-uppercase text-primary fw-bold text-xs mb-1"><span style="color: rgb(255,255,255);">Total users</span></div>
-                                            <div class="text-dark fw-bold h5 mb-0"><span style="color: rgb(255,255,255);"><?php Util::display($user->getUserCount()); ?></span></div>
+                                            <div class="text-uppercase text-primary fw-bold text-xs mb-1"><span style="color: rgb(255,255,255);">subscription</span></div>
+                                            <div class="text-dark fw-bold h5 mb-0"><span style="color: rgb(255,255,255);">
+                                                    <?php
+                                                    if ($sub > 0) {
+                                                        if ($sub > 0 && $sub > 1000) {
+                                                            Util::display('Lifetime');
+                                                        } else {
+                                                            Util::display($sub . ' days');
+                                                        }
+                                                    } else {
+                                                        Util::display('0 days');
+                                                    }
+
+                                                    ?></span></div>
                                         </div>
-                                        <div class="col-auto"><i class="fas fa-user-friends fa-2x text-gray-300" style="color: rgb(200,200,200)!important;"></i></div>
+                                        <div class="col-auto"><i class="fas fa-database fa-2x text-gray-300" style="color: rgb(200,200,200)!important;"></i></div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-6 col-xl-3 mb-4">
-                            <div class="card shadow border-start-primary py-2" style="background: rgb(37,41,53);border-style: none;">
-                                <div class="card-body">
-                                    <div class="row align-items-center no-gutters">
-                                        <div class="col me-2">
-                                            <div class="text-uppercase text-primary fw-bold text-xs mb-1"><span style="color: rgb(255,255,255);">Active subs</span></div>
-                                            <div class="text-dark fw-bold h5 mb-0"><span><?php Util::display($user->getActiveUserCount()); ?></span></div>
-                                        </div>
-                                        <div class="col-auto"><i class="fas fa-file-invoice-dollar fa-2x text-gray-300" style="color: rgb(200,200,200)!important;"></i></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-xl-3 mb-4">
-                            <div class="card shadow border-start-primary py-2" style="background: rgb(37,41,53);border-style: none;">
-                                <div class="card-body">
-                                    <div class="row align-items-center no-gutters">
-                                        <div class="col me-2">
-                                            <div class="text-uppercase text-primary fw-bold text-xs mb-1"><span style="color: rgb(255,255,255);">banned users</span></div>
-                                            <div class="text-dark fw-bold h5 mb-0"><span style="color: rgb(255,255,255);"><?php Util::display($user->getBannedUserCount()); ?></span></div>
-                                        </div>
-                                        <div class="col-auto"><i class="fas fa-ban fa-2x text-gray-300" style="color: rgb(200,200,200)!important;"></i></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-xl-3 mb-4">
-                            <div class="card shadow border-start-primary py-2" style="background: rgb(37,41,53);border-style: none;">
-                                <div class="card-body">
-                                    <div class="row align-items-center no-gutters">
-                                        <div class="col me-2">
-                                            <div class="text-uppercase text-primary fw-bold text-xs mb-1"><span style="color: rgb(255,255,255);">latest user</span></div>
-                                            <div class="text-dark fw-bold h5 mb-0"><span style="color: rgb(255,255,255);"><?php Util::display($user->getNewUser()); ?></span></div>
-                                        </div>
-                                        <div class="col-auto"><i class="fas fa-clock fa-2x text-gray-300" style="color: rgb(200,200,200)!important;"></i></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+
                     </div>
                     <div class="row" data-aos="fade-down" data-aos-duration="400">
                         <div class="col-lg-6 mb-4">
-                            <div class="card shadow mb-4" style="border-style: none;background: rgb(37,41,53);">
+                            <div class="card shadow mb-4" style="border-style: none;background: rgb(37,41,53);max-width: 664px;">
                                 <div class="card-header py-3" style="border-style: none;background: rgb(37,41,53);">
-                                    <h6 class="text-primary fw-bold m-0" style="color: rgb(255,255,255)!important;">Set values</h6>
+                                    <h6 class="text-primary fw-bold m-0" style="color: rgb(255,255,255)!important;">News</h6>
                                 </div>
-                                <form method="POST" action="<?php Util::display($_SERVER['PHP_SELF']); ?>">
-                                    <ul class="list-group list-group-flush" style="background: rgb(37,41,53);">
-                                        <li class="list-group-item" style="background: rgb(37,41,53);">
-
-                                            <div class="row align-items-center no-gutters">
-                                                <div class="col me-2" style="color: rgb(255,255,255);height: 68px;">
-                                                    <div style="margin-bottom: 10px;"><span style="color: rgb(255,255,255);margin-bottom: 0px;">Set version</span></div><input type="text" id="username" placeholder="2.2" name="version" style="background: #121421;border-style: none;outline: none;color: rgb(255,255,255);border-radius: 5px;padding-left: 5px;padding-right: 5px;margin-top: -4px;">
-                                                </div>
-                                            </div>
-                                            <div class="row align-items-center no-gutters">
-                                                <div class="col me-2" style="color: rgb(255,255,255);height: 68px;"><button type="submit" value="submit" class="btn btn-success" name="cheatVersion" style="font-size: 12px;color: rgb(255,255,255);margin-top: 7px;">Update</button><button class="btn btn-success" type="submit" value="submit" name="cheatStatus" style="font-size: 12px;color: rgb(255,255,255);margin-top: 7px;margin-left: 10px;">Detected</button><button class="btn btn-success" name="cheatMaint" type="submit" value="submit" style="font-size: 12px;color: rgb(255,255,255);margin-top: 7px;margin-left: 10px;">Maintenace</button></div>
-                                            </div>
-
-                                        </li>
-                                    </ul>
-                                </form>
-
-                            </div>
-                            <form action="<?php Util::display($_SERVER['PHP_SELF']); ?>" method="post">
                                 <ul class="list-group list-group-flush" style="background: rgb(37,41,53);">
                                     <li class="list-group-item" style="background: rgb(37,41,53);">
-
                                         <div class="row align-items-center no-gutters">
-                                            <div class="col me-2" style="color: rgb(255,255,255);height: 68px;">
-
-                                                <input autocomplete="off" type="text" name="msg" maxlength="255" placeholder="News" required style="background: #121421;border-style: none;outline: none;color: rgb(255,255,255);border-radius: 5px;padding-left: 5px;padding-right: 5px;margin-top: -4px;">
-                                                <br>
-
-                                                <button type="submit" value="Send" name="sendmsg" class="btn btn-success" style="font-size: 12px;color: rgb(255,255,255);margin-top: 7px;">Update</button>
+                                            <div class="col me-2" style="color: rgb(255,255,255);">
+                                                <h6 class="mb-0"><strong><?php  Util::display( $user->getusernews())?></strong></h6>
                                             </div>
                                         </div>
                                     </li>
                                 </ul>
-                            </form>
+                            </div>
                         </div>
+                        <?php if ($user->getSubStatus() > 0) : ?>
+                            <div class="col-lg-6 mb-4" id="DOWNLOADCOL">
+                                <div class="card shadow mb-4" style="border-style: none;background: rgb(37,41,53);max-width: 664px;">
+                                    <div class="card-header py-3" style="border-style: none;background: rgb(37,41,53);">
+                                        <h6 class="text-primary fw-bold m-0" style="color: rgb(255,255,255)!important;">Loader</h6>
+                                    </div>
+                                    <ul class="list-group list-group-flush" style="background: rgb(37,41,53);">
+                                        <li class="list-group-item" style="background: rgb(37,41,53);">
+                                            <div class="row align-items-center no-gutters">
+                                                <a style="margin-left: 0px;font-size: 12px;color: rgb(255,255,255);margin-bottom: 10px;" class='nav-link' href=download.php>Download <i class="fas fa-download"></i></a>
+
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        <?php endif; ?>
                     </div>
+
                 </div>
             </div>
         </div>
     </div>
-    <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
-    <script src="../assets/js/bs-init.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/bs-init.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
-    <script src="../assets/js/theme.js"></script>
+    <script src="assets/js/theme.js"></script>
+    <script src="assets/js/jquery.js"></script>
 </body>
 
 </html>
