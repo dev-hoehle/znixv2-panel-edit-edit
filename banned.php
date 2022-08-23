77365f0c497e378bda7197962281c9e7f95e2114
anditv21 <75949923+anditv21@users.noreply.github.com>
2022-08-23 22:16:56 +0200

    Revert "🧹 Cleanup"
    This reverts commit d2cc3c2fc4e86649302a088c94c9eb062d7fd32a.

--- banned.php
+++ banned.php
@@ -15,9 +15,12 @@
    <meta charset="utf-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
    <title>Banned - Brand</title>
-   <link rel="icon" type="image/png" href="favicon.png">
+   <link rel="icon" type="image/x-icon" href="../../favicon.ico">
    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
    <link rel="stylesheet"href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
+   <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
+   <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+   <link rel="stylesheet" href="assets/fonts/fontawesome5-overrides.min.css">
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
    <link rel="stylesheet" href="assets/css/untitled.css">
 </head>
--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,12 +1,12 @@
 <footer class="fixed-bottom text-right pb-3 pr-5">
 	<small>
-		<span class="align-middle text-decoration-none">&nbsp&nbsp&nbsp View this project on <a href="https://github.com/anditv21/znixv2-panel-edit-edit" target="_blank">GitHub</a></span>
+		<span class="align-middle text-decoration-none">&nbsp&nbsp&nbsp View this project on <a href="https://github.com/anditv21/znixv2-panel-edit-edit" target="_blank">Github</a></span>
 	</small>
 </footer>
 
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="<?= SUB_DIR ?>/assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="<?= SUB_DIR ?>/bootstrap/js/bootstrap.min.js"></script>
 </body>
 
 </html>
