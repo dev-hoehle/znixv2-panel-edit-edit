1b747305de7c45f76f0442020c772152f72c032d
anditv21 <75949923+anditv21@users.noreply.github.com>
2022-08-24 05:50:56 +0200

    🔧 Visual changes

--- admin/index.php
+++ admin/index.php
@@ -236,7 +236,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                                     <div class="row align-items-center no-gutters">
                                         <div class="col me-2">
                                             <div class="text-uppercase text-primary fw-bold text-xs mb-1"><span style="color: rgb(255,255,255);">frozen</span></div>
-                                            <div class="text-dark fw-bold h5 mb-0"><span><?php Util::display($cheat->getCheatData()->frozen); ?></span></div>
+                                            <div class="text-dark fw-bold h5 mb-0"><span><?php
+                                            if ($cheat->getCheatData()->frozen == 1) 
+                                            {
+                                                Util::display("True");
+                                            }
+                                            else
+                                            {
+                                                Util::display("False");
+                                            } ?></span></div>
                                         </div>
                                         <div class="col-auto"><i class="fas fa-snowflake fa-2x text-gray-300" style="color: rgb(200,200,200)!important;"></i></div>
                                     </div>
